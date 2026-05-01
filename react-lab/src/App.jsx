393bfd9d50cbb6de297b0a6028373912f321a930
KasunInd27 <kasunindrajith27@gmail.com>
2026-05-01 23:08:55 +0530

    Completed Exercise 02

--- react-lab/src/App.jsx
+++ react-lab/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Greeting from "./components/Greeting";
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <>
       <Greeting />
+      <NavBar />
     </>
   )
 }
